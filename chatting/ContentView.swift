a3b1215c2452bce0b7671f2b9b607f2852234e86
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-07-26 18:06:56 +0900

    Initial Commit

---
+++ chatting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chatting
+//
+//  Created by 곽보선 on 2021/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chatting/chattingApp.swift
@@ -0,0 +1,17 @@
+//
+//  chattingApp.swift
+//  chatting
+//
+//  Created by 곽보선 on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct chattingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
